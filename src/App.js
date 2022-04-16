de58419dea34e56e799c67fb231f3e3db087f696
Jonathan Kromer <jdkromer@gmail.com>
2022-04-15 23:00:23 -0400

    Defining & Using Routes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/ => Component A
+// our-domain.com/products => Component B
